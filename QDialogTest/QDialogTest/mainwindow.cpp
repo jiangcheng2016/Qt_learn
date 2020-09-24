3c067e758cf6d00066c4e0379afee826a1ea2057
jiangchengcheng <jianglvforever@163.com>
2020-09-24 21:32:14 +0800

    QDialog

--- QDialogTest/QDialogTest/mainwindow.cpp
+++ QDialogTest/QDialogTest/mainwindow.cpp
@@ -1,11 +1,40 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDialog>
+#include <QDebug>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //点击新建 弹出一个对话框
+    connect(ui->actionnew,&QAction::triggered,[=](){
+        //对话框 Dialog  分类
+        // 模态对话框（不可以对其他窗口操作)      非模态对话框(可以对其他窗口进行操作）
+
+        //模态创建
+//        QDialog dig(this);
+//        dig.exec();
+
+//        qDebug() << "aaa";
+
+        //非模态创建
+
+        /*
+
+        QDialog dig2(this);
+        dig2.show();
+        如果使用这种情况，那么dig2在栈上面，当执行完 dig2.show() 以后窗口就会关闭，所以看不到
+        此时，把它放到堆上面
+        */
+        QDialog * dig2 = new QDialog(this);
+        dig2->show();
+        dig2->setAttribute(Qt::WA_DeleteOnClose);   //55号属性
+        qDebug() << "fei mo tai";
+
+    });
 }
 
 MainWindow::~MainWindow()
