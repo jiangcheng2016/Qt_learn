41a1131b2c943c8531dbeeca7968297006f5dddf
jiangchengcheng <jianglvforever@163.com>
2020-09-24 21:31:09 +0800

    QResource

---
+++ QResourceTest/QResourceTest/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //使用添加Qt资源 ": + 前缀名 + 文件名"
+    ui->actionnew->setIcon(QIcon(":/Image/Luffy.png"));
+    ui->actionopen->setIcon(QIcon(":/Image/LuffyQ.png"));
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
