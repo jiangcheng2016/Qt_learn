e1baaed1dd02f32bae2c0f9397e4b0dac93fd0f3
jiangchengcheng <jianglvforever@163.com>
2020-09-28 20:53:09 +0800

    qtEvent

---
+++ 03/QtEvent/mylabel.cpp
@@ -0,0 +1,49 @@
+﻿#include "mylabel.h"
+#include <QDebug>
+#include <QString>
+#include <QMouseEvent>
+
+MyLabel::MyLabel(QWidget *parent) : QLabel(parent)
+{
+    //设置鼠标移动监控
+    setMouseTracking(true);
+
+}
+
+//鼠标进入事件
+void MyLabel::enterEvent(QEvent *event){
+    //qDebug() << "鼠标进入了区域";
+}
+
+//鼠标离开事件
+void MyLabel::leaveEvent(QEvent *event){
+    //qDebug() << "鼠标离开了区域";
+}
+
+
+//鼠标按压事件
+void MyLabel::mousePressEvent(QMouseEvent *ev){
+    if (ev->button() == Qt::LeftButton)
+    {
+        QString str = QString("鼠标按压的位置为： x =%1 y = %2 globalx = %3  global4 = %4 ").arg(ev->x()).arg(ev->y()).arg(ev->globalX()).arg(ev->globalY());
+        qDebug() << str ;
+    }
+}
+
+//鼠标释放事
+void MyLabel::mouseReleaseEvent(QMouseEvent *ev){
+    if (ev->button() == Qt::LeftButton)
+    {
+        QString str = QString("鼠标释放了 x =%1 y = %2 globalx = %3  global4 = %4 ").arg(ev->x()).arg(ev->y()).arg(ev->globalX()).arg(ev->globalY());
+        qDebug() << str ;
+    }
+}
+
+//鼠标移动事件
+void MyLabel::mouseMoveEvent(QMouseEvent *ev){
+    //if (ev->buttons() & Qt::LeftButton)
+    //{
+        QString str = QString("鼠标移动了 x =%1 y = %2 globalx = %3  global4 = %4 ").arg(ev->x()).arg(ev->y()).arg(ev->globalX()).arg(ev->globalY());
+        qDebug() << str ;
+    //}
+}
---
+++ 03/QtEvent/mylabel.h
@@ -0,0 +1,33 @@
+﻿#ifndef MYLABEL_H
+#define MYLABEL_H
+
+#include <QLabel>
+
+class MyLabel : public QLabel
+{
+    Q_OBJECT
+public:
+    explicit MyLabel(QWidget *parent = nullptr);
+
+    //鼠标进入事件
+    void enterEvent(QEvent *event);
+
+    //鼠标离开事件
+    void leaveEvent(QEvent *event);
+
+
+    //鼠标按压事件
+    virtual void mousePressEvent(QMouseEvent *ev);
+
+    //鼠标释放事件
+    virtual void mouseReleaseEvent(QMouseEvent *ev);
+
+    //鼠标按压事件
+    virtual void mouseMoveEvent(QMouseEvent *ev);
+
+
+signals:
+
+};
+
+#endif // MYLABEL_H
