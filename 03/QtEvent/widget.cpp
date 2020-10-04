13dc6e05bc0e8cb20ddf887bea5d837a2e992010
jiangchengcheng <jianglvforever@163.com>
2020-10-04 11:29:06 +0800

    timerEvent

--- 03/QtEvent/widget.cpp
+++ 03/QtEvent/widget.cpp
@@ -1,4 +1,4 @@
-#include "widget.h"
+﻿#include "widget.h"
 #include "ui_widget.h"
 
 Widget::Widget(QWidget *parent)
@@ -6,7 +6,30 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    //启动计时器1
+    id1 = startTimer(1000);
+
+    //计时器2
+    id2 = startTimer(2000);
 }
+
+
+void Widget::timerEvent(QTimerEvent *ev)
+{
+    //lable2
+    if (ev->timerId() == id1)
+    {
+        static int num = 1;
+        ui->label_2->setText(QString().number(num ++));
+    }
+    //lable3
+    if (ev->timerId() == id2)
+    {    static int num2 = 1;
+        ui->label_3->setText(QString().number(num2 ++));
+    }
+}
+
 
 Widget::~Widget()
 {
